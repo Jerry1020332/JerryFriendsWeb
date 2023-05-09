0572ee404a0e29bb81ff9a6f9128a86c6452f453
Jerry1020332 <jerry1020332@gmail.com>
2023-05-09 14:08:58 +0800

    do not

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+<<<<<<< Updated upstream
           Edit <code>src/App.js</code> and do not save to reload.
+=======
+          Edit <code>src/App.js</code> and don't save to reload.
+>>>>>>> Stashed changes
         </p>
         <a
           className="App-link"
